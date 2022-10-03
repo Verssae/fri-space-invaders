36e301f0eb95374f944a44be64fe0ca7fa43cd98
kimujae <dnwo0103@naver.com>
2022-10-03 17:47:00 +0900

    [UPDATE]engine/ItemManager 주석추가 및 작업할당

--- src/engine/ItemManager.java
+++ src/engine/ItemManager.java
@@ -38,12 +38,19 @@ public class ItemManager {
     }
 
         public void assignHasItem(EnemyShipFormation enemyshipformation) {
-
+            //이예나
+            /*enemyshipformation의 enemyship에 0,1 값 할당
+              1이면 hasItem
+              30퍼센트의 적이 hasItem값 할당
+             */
 
         }
 
         public void assignItem (EnemyShipFormation enemyshipformation, int i, int j){
-
+            //노은솔
+            /*enemyshipformation의 enemyship의 hasItem값이 1이면 item_queue에서 아이템타입 할당
+             */
+            //
         }
 
 
@@ -55,13 +62,17 @@ public class ItemManager {
 
 
         private void addlist () {
-
-
+            //박용수
+            /*
+            아이템타입의 리스트
+             */
         }
 
         private void addqueue () {
-
-
+            //박용수
+            /*
+            아이템타입들이 셔플된 큐
+             */
         }
 
 

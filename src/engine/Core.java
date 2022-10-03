43c0a6c1f61f71f9d95fdb36328bc29c54cf3d02
LeeMingu <qazzaq1541@gmail.com>
2022-10-04 00:26:39 +0900

    [fix] GameState 갱신 안되는 버그 fix

--- src/engine/Core.java
+++ src/engine/Core.java
@@ -147,13 +147,13 @@ public final class Core {
 					frame.setScreen(currentScreen);
 					LOGGER.info("Closing game screen.");
 
+					gameState = ((GameScreen) currentScreen).getGameState();
+
 					currentScreen = new GameSaveScreen(gameState, width, height, FPS);
 					returnCode = frame.setScreen(currentScreen);
 					if (returnCode == 2){
 
 					}
-
-					gameState = ((GameScreen) currentScreen).getGameState();
 
 					gameState = new GameState(gameState.getLevel() + 1,
 							gameState.getScore(),

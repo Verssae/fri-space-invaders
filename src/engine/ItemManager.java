abad2c1c109d09100f2c2fcc2ab40da04133adbc
Noeunsol <nes70117011@gmail.com>
2022-10-03 19:14:18 +0900

    Merge branch 'nes_bulletspeed_extralife' of C:\Users\82102\Desktop\spaceinvaders\fri-space-invaders with conflicts.

--- src/engine/ItemManager.java
+++ src/engine/ItemManager.java
@@ -43,6 +43,33 @@ public class ItemManager {
               1이면 hasItem
               30퍼센트의 적이 hasItem값 할당
              */
+            enemyshipssize = enemyshipformation.getEnemyShip().size();
+            final int nShipsWide = enemyshipformation.getnshipsWide();
+            final int nShipsHigh = enemyshipformation.getnShipsHigh();
+
+            Integer[][] random = new Integer[nShipsWide][nShipsHigh];
+
+
+            for (int i = 0; i < nShipsWide; i++) {
+                for (int j = 0; j < nShipsHigh; j++) {
+                    if (Math.random()*10 + 1 < 2.1) {
+                        random[i][j] = 1;
+                    } else random[i][j] = 0;
+                }
+            }
+
+            for (int i = 0; i < nShipsWide; i++) {
+                for (int j = 0; j < nShipsHigh; j++) {
+                    if (random[i][j] == 1) {
+                        // System.out.println("랜덤 1인 경우"); //디버그용
+                        enemyshipformation.getEnemyShip().get(i).get(j).setHasItem(1);
+                        this. assignItem(enemyshipformation, i , j);
+                    }
+                    else enemyshipformation.getEnemyShip().get(i).get(j).setHasItem(0);
+                    System.out.println(enemyshipformation.getEnemyShip().get(i).get(j).getHasItem());//로그추가
+                }
+
+            }
 
         }
 
@@ -50,8 +77,8 @@ public class ItemManager {
             //노은솔
             /*enemyshipformation의 enemyship의 hasItem값이 1이면 item_queue에서 아이템타입 할당
              */
-            //
-          //  ggggg
+
+
         }
 
 
@@ -67,6 +94,12 @@ public class ItemManager {
             /*
             아이템타입의 리스트
              */
+            item_list.add(pointupitem);
+            item_list.add(speedupitem);
+            item_list.add(shielditem);
+            item_list.add(extralifeitem);
+            item_list.add(bulletspeeditem);
+
         }
 
         private void addqueue () {
@@ -74,6 +107,11 @@ public class ItemManager {
             /*
             아이템타입들이 셔플된 큐
              */
+            item_queue.add(this.item_list.get(0));
+            item_queue.add(this.item_list.get(1));
+            item_queue.add(this.item_list.get(2));
+            item_queue.add(this.item_list.get(3));
+            item_queue.add(this.item_list.get(4));
         }
 
 

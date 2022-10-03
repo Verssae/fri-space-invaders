298a4a7e20dfbf0640a6a0e657992657498bc3bd
iwtkmn0219 <iwtkmn0219@naver.com>
2022-10-04 05:40:17 +0900

    feat: Add coins.java

---
+++ src/engine/coins.java
@@ -0,0 +1,10 @@
+package engine;
+
+public class coins {
+    private int coins;
+    public coins(final int coins) {
+        this.coins = coins;
+    }
+
+    public final int getCoins() { return this.coins; }
+}

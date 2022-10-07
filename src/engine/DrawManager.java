f025459a6c05bdb793881afcb7405360e6041e6b
kimujae <dnwo0103@naver.com>
2022-10-07 10:55:10 +0900

    [UPDATE] engine/DrawManager, res/graphics, res/graphics1,res/graphics2 , entity/Item 아이템 모양 및 색깔 변경

--- src/engine/DrawManager.java
+++ src/engine/DrawManager.java
@@ -112,8 +112,8 @@ public final class DrawManager {
 			spriteMap.put(SpriteType.EnemyShipC2, new boolean[12][8]);
 			spriteMap.put(SpriteType.EnemyShipSpecial, new boolean[16][7]);
 			spriteMap.put(SpriteType.Explosion, new boolean[13][7]);
-			spriteMap.put(SpriteType.ItemDrop, new boolean[3][5]);
-			spriteMap.put(SpriteType.ItemGet, new boolean[3][5]);
+			spriteMap.put(SpriteType.ItemDrop, new boolean[5][5]);
+			spriteMap.put(SpriteType.ItemGet, new boolean[5][5]);
 
 			fileManager.loadSprite(spriteMap);
 			logger.info("Finished loading the sprites.");

92d93012d21b6b0cc96dc81853deb6c0024c2050
yppp33 <ysp051@hanyang.ac.kr>
2022-10-14 17:02:16 +0900

    [FIX]ItemManager -.아이템리스트 수정

--- src/engine/ItemManager.java
+++ src/engine/ItemManager.java
@@ -84,16 +84,12 @@ public class ItemManager {
             /*
             아이템타입의 리스트
              */
-//            item_list.add(pointupitem);
+            item_list.add(pointupitem);
             item_list.add(speedupitem);
-//            item_list.add(shielditem);
-//            item_list.add(extralifeitem);
-//            item_list.add(bulletspeeditem);
+            item_list.add(shielditem);
+            item_list.add(extralifeitem);
+            item_list.add(bulletspeeditem);
             item_list.add(machinegun);
-            item_list.add(speedupitem);
-            item_list.add(machinegun);
-            item_list.add(speedupitem);
-            item_list.add(speedupitem);
         }
 
 

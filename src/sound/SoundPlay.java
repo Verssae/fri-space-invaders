14245d21e42ab78128d296f2b4ac931326304d20
Kimbogyung <novo4756@gmail.com>
2022-10-14 15:04:03 +0900

    feat/set volume

--- src/sound/SoundPlay.java
+++ src/sound/SoundPlay.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 public class SoundPlay{
     private static SoundPlay instance;
     private int effectVolume = 70;
+    private int bgmVolume = 50;
+
     private SoundPlay(){}
     public static SoundPlay getInstance(){
         if(instance == null) instance = new SoundPlay();
@@ -41,6 +43,22 @@ public class SoundPlay{
         se.setVolume(effectVolume);
         se.play();
         se.stop();
+    }
+
+    public void setEffectVolume(int value){
+        this.effectVolume = value;
+    }
+
+    public void setBgmVolume(int value){
+        this.bgmVolume = value;
+    }
+
+    public int getEffectVolume(){
+        return effectVolume;
+    }
+
+    public int getBgmVolume(){
+        return bgmVolume;
     }
 
 }

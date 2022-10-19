dfdd991a478a3c0ea8d519a721d8aa8181a4bef0
kcyoow <46476820+kcyoow@users.noreply.github.com>
2022-10-19 15:10:09 +0000

    (ReCommit) ADD ingame BGM & MODIFY SoundPlay

--- src/sound/SoundPlay.java
+++ src/sound/SoundPlay.java
@@ -48,7 +48,7 @@ public class SoundPlay{
     //SoundPlay.getInstance().Play("파일명.wav"); 로 음악 재생
     //음악은 res폴더안에
     public void playBgm(String filename){
-        if(sb == null){
+        if(sb == null || !sb.bgmClip.isActive()){
             sb = new SoundBgm(filename);
             sb.bgmVolume(bgmVolume);
             sb.play();
@@ -56,7 +56,7 @@ public class SoundPlay{
     }
 
     public void stopBgm(){
-        sb.stop();   
+        sb.stop();
     }
 
 

136d4e607765077e6ab3fe3a943563f30ced0d3d
kcyoow <46476820+kcyoow@users.noreply.github.com>
2022-10-19 13:31:41 +0000

    merging GameScreen to main

--- src/screen/GameScreen.java
+++ src/screen/GameScreen.java
@@ -127,7 +127,6 @@ public class GameScreen extends Screen {
 			final GameSettings gameSettings, final boolean bonusLife,
 			final int width, final int height, final int fps) {
 		super(width, height, fps);
-
 		this.gameSettings = gameSettings;
 		this.bonusLife = bonusLife;
 		this.level = gameState.getLevel();
@@ -209,7 +208,6 @@ public class GameScreen extends Screen {
 	 */
 	public final int run() {
 		super.run();
-
 		SoundPlay.getInstance().stopBgm();
 		this.score += LIFE_SCORE * (this.lives - 1);
 		this.logger.info("Screen cleared with a score of " + this.score); // 정상 출력

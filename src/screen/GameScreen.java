e504ffdb03f9d2520cfb8ec650d1eac31fe9703a
kcyoow <46476820+kcyoow@users.noreply.github.com>
2022-10-19 15:01:06 +0000

    Revert "ADD ingame BGM & MODIFY SoundPlay"
    This reverts commit 14cb420f82e290ee86de6e1f9d4caeca0f7c3458, reversing
    changes made to 9c15b6aa6c4ed54054e4e5c2238c913780806ac0.

--- src/screen/GameScreen.java
+++ src/screen/GameScreen.java
@@ -129,6 +129,7 @@ public class GameScreen extends Screen {
 			final GameSettings gameSettings, final boolean bonusLife,
 			final int width, final int height, final int fps) {
 		super(width, height, fps);
+
 		this.gameSettings = gameSettings;
 		this.bonusLife = bonusLife;
 		this.level = gameState.getLevel();
@@ -136,9 +137,6 @@ public class GameScreen extends Screen {
 		this.lives = gameState.getLivesRemaining();
 		if (this.bonusLife)
 			this.lives++;
-		if(lives == 0){
-			SoundPlay.getInstance().stopBgm();
-		}
 		this.bulletsShot = gameState.getBulletsShot();
 		this.shipsDestroyed = gameState.getShipsDestroyed();
 
@@ -155,16 +153,6 @@ public class GameScreen extends Screen {
 	 */
 	public final void initialize() {
 		super.initialize();
-
-		TimerTask task = new TimerTask() {
-			@Override
-			public void run() {
-				SoundPlay.getInstance().play(SoundType.mainGameBgm);
-			}
-		};
-		Timer timer = new Timer("Timer");
-		long delay = 5800L;
-		timer.schedule(task, delay);
 
 		enemyShipFormation = new EnemyShipFormation(this.gameSettings);
 		itemmanager.assignHasItem(enemyShipFormation);
@@ -210,7 +198,7 @@ public class GameScreen extends Screen {
 	 */
 	public final int run() {
 		super.run();
-		SoundPlay.getInstance().stopBgm();
+
 		this.score += LIFE_SCORE * (this.lives - 1);
 		this.logger.info("Screen cleared with a score of " + this.score); // 정상 출력
 		return this.returnCode;

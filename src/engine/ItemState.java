7f87a2f3e2a4e9bb1688bdcce835f1ee2b67b0f0
iwtkmn0219 <iwtkmn0219@naver.com>
2022-11-23 20:51:05 +0900

    TEAM-56 <feat: Add ItemState File for manage Item>

---
+++ src/engine/ItemState.java
@@ -0,0 +1,55 @@
+package engine;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.logging.Logger;
+
+/**
+ * Implements an object that Items the persistent game state.
+ *
+ * @author <a href="mailto:iwtkmn0219@naver.com">Minjun Choi</a>
+ *
+ * */
+
+public class ItemState {
+    LinkedHashMap<I_State, Integer> i_state;
+    private static Logger logger;
+    private static ItemState iS;
+    private final FileManager fileManager = FileManager.getInstance();
+
+    private ItemState() {
+        try {
+            this.i_state = fileManager.loadI_State();
+        } catch (IOException e) {
+            logger.warning("Couldn't load i_state!");
+        }
+    }
+
+    public static ItemState getInstance() {
+        if (iS == null)
+            iS = new ItemState();
+        return iS;
+    }
+
+    public int getI_state(I_State key) {
+        return i_state.get(key);
+    }
+
+    public void gainI_state(I_State key, int value) {
+        i_state.replace(key, getI_state(key) + value);
+        try {
+            fileManager.saveI_State(i_state);
+        } catch (IOException e) {
+            logger.info(e.toString());
+        }
+    }
+
+    public void setI_state(I_State key, int value) {
+        i_state.replace(key, value);
+        try {
+            fileManager.saveI_State(i_state);
+        } catch (IOException e) {
+            logger.info(e.toString());
+        }
+    }
+}
